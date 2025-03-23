818d625c690f55e7f8400c7bea02c0c528f7fc3c
koitoyou <43528137+koitoyou@users.noreply.github.com>
2025-03-23 16:43:07 +0900

    サイトでのインクルードを行うJSとHTMLファイルを登録

---
+++ default.js
@@ -0,0 +1,18 @@
+const headerInclude = async (tag,id) => {
+  if ( $(tag + id).length == 1 ) {
+    fetch(`/include/${tag}.html`)
+    .then(response => {
+      if ( !response.ok ) {
+        throw new Error(`ResponseStatus: ${response.status}`);
+      }
+      return response.text()
+    }).then(data => {
+      console.debug(data);
+      $(tag + id).html(data);
+    })
+    .catch(error => { console.debug(error) });
+  }
+}
+headerInclude("header","#header");
+headerInclude("footer","#footer");
+
